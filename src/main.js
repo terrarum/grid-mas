b41db40e6ce2aaf733509e83b3373c3e282b7605
James Fidler <terrarum@gmail.com>
2016-02-28 21:08:47 +0000

    Added CSS.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
+// Load CSS.
+require("./main.css");
+
+// Import modules.
 import Gameloop from './modules/gameloop';
 
-let update = function update(dt) {
-    console.log('h')
-};
-
-let render = function render(dt) {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-};
-
-
-let ctx, canvas;
-
-// Get the canvas.
-ctx = document.querySelector('.js-canvas').getContext('2d');
-canvas = ctx.canvas;
+// Set canvas and canvas context references.
+const ctx = document.querySelector('.js-canvas').getContext('2d'),
+    canvas = ctx.canvas;
 
 // Set the canvas size.
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Update function.
+let update = function update(dt) {
 
+};
+
+// Render function.
+let render = function render(dt) {
+    // Clear screen.
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+};
+
+// Create new game loop.
 const gl = new Gameloop(update, render);
 
+// Start game loop.
 gl.start();
