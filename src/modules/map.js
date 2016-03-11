5090a3a22d9c57b7aac6349d8155685bfc303a98
James Fidler <james.fidler@immediate.co.uk>
2016-03-11 16:25:11 +0000

    Map now rendering correctly.

--- src/modules/map.js
+++ src/modules/map.js
@@ -4,8 +4,12 @@ class Map {
     constructor(scene) {
         this.scene = scene;
 
-        document.addEventListener("MAP:LOADED", (event) => {
+        document.addEventListener("MAP:DATA:LOADED", (event) => {
             this.processMap();
+        });
+
+        document.addEventListener("MAP:TILESET:LOADED", (event) => {
+            this.renderMap();
         });
     }
 
@@ -20,7 +24,7 @@ class Map {
             this.scene.map = JSON.parse(response);
 
             // Fire MAP:LOADED event.
-            document.dispatchEvent(new Event('MAP:LOADED'));
+            document.dispatchEvent(new Event('MAP:DATA:LOADED'));
 
         }, (error) => {
             console.error("Failed to load map `" + name + "` from `" + path + "`.");
@@ -28,25 +32,73 @@ class Map {
     }
 
     processMap() {
-        // Load tilesets.
-        console.log(this.scene);
-        const tilesetImage = this.scene.map.tilesets[0];
-        const tilesetImageUrl = tilesetImage.image.split('/').pop();
-        console.log(tilesetImage, tilesetImageUrl);
+        // Load tileset.
+        this.tileset = this.scene.map.tilesets[0];
+        const tilesetImageUrl = this.tileset.image.split('/').pop();
 
-        this.scene.ctx.canvas.width = tilesetImage.imagewidth;
-        this.scene.ctx.canvas.height = tilesetImage.imageheight;
+        this.tileset.img = new Image();
+        this.tileset.img.src = '/grid-mas/assets/spritesheets/' + tilesetImageUrl;
+        this.tileset.img.onload = () => {
+            document.dispatchEvent(new Event('MAP:TILESET:LOADED'));
+        };
+    }
 
-        tilesetImage.img = new Image();
-        tilesetImage.img.src = '/grid-mas/assets/spritesheets/' + tilesetImageUrl;
-        tilesetImage.img.onload = () => {
-            this.scene.ctx.drawImage(tilesetImage.img, 0, 0);
+    /**
+     * Returns coordinates on tileset of given cell id.
+     * @param tileId {number} - id of tileset cell to render.
+     */
+    getTileCoords(tileId) {
+        // ID's are calculated left to right, top to bottom.
+        // CellID 63 is row 2 column 6 on a 57 column tileset.
+        const cols = this.tileset.columns;
+        const spacing = this.tileset.spacing;
+        const tilesize = this.tileset.tilewidth;
+
+        const row = Math.floor(tileId / cols);
+        const col = tileId % cols;
+
+        return {
+            x: (col - 1) * tilesize + (spacing * col) - 1,
+            y: row * tilesize + (spacing * row)
+        }
+    }
+
+    drawCell(cellId, tileId) {
+
+        const coords = this.getTileCoords(tileId);
+
+        const cols = this.scene.map.width;
+        const tilesize = this.scene.map.tilewidth;
+
+        const row = Math.floor(cellId / cols);
+        const col = cellId % cols;
+
+        const dest = {
+            x: col * tilesize,
+            y: row * tilesize
         };
 
+        this.scene.ctx.drawImage(this.tileset.img, coords.x, coords.y, this.tileset.tilewidth, this.tileset.tileheight, dest.x, dest.y, this.tileset.tilewidth, this.tileset.tileheight);
+    }
 
+    renderMap() {
+        console.log(this.scene.map);
+        // Set canvas size.
+        this.scene.ctx.canvas.width = this.scene.map.width * this.tileset.tilewidth;
+        this.scene.ctx.canvas.height = this.scene.map.height * this.tileset.tileheight;
 
-        // Load image into canvas.
+        const layers = this.scene.map.layers;
+        let layer, tileId;
 
+        // Loop through layers.
+        for (let i = 0, len = layers.length; i < len; i++) {
+            // For each layer, draw each cell.
+            layer = layers[i];
+            for (let j = 0, len = layer.data.length; j < len; j++) {
+                tileId = layer.data[j];
+                this.drawCell(j, tileId)
+            }
+        }
     }
 }
 
