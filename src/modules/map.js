f8c3a5ddff5e87b1df256e5f8c41dbd162afda4e
James Fidler <james.fidler@immediate.co.uk>
2016-03-11 17:15:44 +0000

    Updated map, added comments in map.js about pathfinding plans.

--- src/modules/map.js
+++ src/modules/map.js
@@ -31,16 +31,55 @@ class Map {
         });
     }
 
+    /**
+     * Get the tilesheet image and load it.
+     * Create the pathfinding map?
+     */
     processMap() {
         // Load tileset.
         this.tileset = this.scene.map.tilesets[0];
         const tilesetImageUrl = this.tileset.image.split('/').pop();
 
+        // Load image.
         this.tileset.img = new Image();
         this.tileset.img.src = '/grid-mas/assets/spritesheets/' + tilesetImageUrl;
         this.tileset.img.onload = () => {
             document.dispatchEvent(new Event('MAP:TILESET:LOADED'));
         };
+
+        // Generate pathfinding array.
+        console.log(this.scene.map);
+        // Loop through layers.
+        // If something is in layer Ground and not 0, it is walkable.
+        // If something is in layer Water and not 0, it is walkable and has a cost of 2.
+        // If something is in layer Roads and not 0, it is walkable and has a cost of 0.5.
+        // If something is in layer Obstacles and not 0, it is not walkable.
+
+        // Easystar sets acceptable tiles (so Ground, Water, Roads)
+        // Easystar can set tile cost on tile types.
+        // So, if layers are represented by their tilecost number.
+        // if (layer.properties.movementCost < 10) {
+        //      add to acceptable tiles array
+        //      set tile cost
+        // }
+        // Assuming 10 is an arbitrary cutoff value, where any movement cost equal to or higher than 10 becomes unwalkable.
+
+
+        const layers = this.scene.map.layers;
+        let layer, tileId;
+
+        // Loop through layers.
+        for (let i = 0, len = layers.length; i < len; i++) {
+            // For each layer, draw each cell.
+            layer = layers[i];
+            console.log(layer.properties.movementCost)
+            for (let j = 0, len = layer.data.length; j < len; j++) {
+                tileId = layer.data[j];
+                if (tileId !== 0) {
+
+                }
+            }
+        }
     }
 
     /**
@@ -63,6 +102,11 @@ class Map {
         }
     }
 
+    /**
+     * Draw the given tile at the given cell.
+     * @param cellId {number} - cell to draw tile at.
+     * @param tileId {number} - tile to draw.
+     */
     drawCell(cellId, tileId) {
 
         const coords = this.getTileCoords(tileId);
@@ -81,8 +125,10 @@ class Map {
         this.scene.ctx.drawImage(this.tileset.img, coords.x, coords.y, this.tileset.tilewidth, this.tileset.tileheight, dest.x, dest.y, this.tileset.tilewidth, this.tileset.tileheight);
     }
 
+    /**
+     * Render the map.
+     */
     renderMap() {
-        console.log(this.scene.map);
         // Set canvas size.
         this.scene.ctx.canvas.width = this.scene.map.width * this.tileset.tilewidth;
         this.scene.ctx.canvas.height = this.scene.map.height * this.tileset.tileheight;
