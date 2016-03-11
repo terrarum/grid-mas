c37df6c2085276ce8a885157db149bd3dc580ab0
James Fidler <james.fidler@immediate.co.uk>
2016-03-11 14:23:40 +0000

    Rendering entire tileset onto canvas.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ require("./main.css");
 
 // Import modules.
 import GameLoop from './modules/gameloop';
-import Grid from './modules/grid';
+//import Grid from './modules/grid';
+import Map from './modules/map';
+
 import Rabbit from './entities/rabbit';
+
 
 const scene = {
     ctx: null,
-    grid: null
+    //grid: null
+    map: null
 };
 
 // Wait for DOM ready.
@@ -16,36 +20,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Set canvas and canvas context references.
     scene.ctx = document.querySelector('.js-canvas').getContext('2d');
 
+    scene.map = new Map(scene);
+
+    scene.map.load('map1');
+
     // Set the canvas size.
-    scene.ctx.canvas.width = window.innerWidth;
-    scene.ctx.canvas.height = window.innerHeight;
+    //scene.ctx.canvas.width = window.innerWidth;
+    //scene.ctx.canvas.height = window.innerHeight;
 
     // Create new grid.
-    scene.grid = new Grid(scene);
+    //scene.grid = new Grid(scene);
 
-    let rabbit = new Rabbit(scene);
-    rabbit.setPosition(5, 5);
-
-    // Update function.
-    let update = function update(dt) {
-        rabbit.update(dt);
-    };
-
-    // Render function.
-    let render = function render(dt) {
-        // Clear screen.
-        scene.ctx.clearRect(0, 0, scene.ctx.canvas.width, scene.ctx.canvas.height);
-
-        // Render grid.
-        scene.grid.render();
-
-        // Render entities.
-        rabbit.render();
-    };
-
-    // Create new game loop.
-    const gl = new GameLoop(update, render);
-
-    // Start game loop.
-    gl.start();
+    //let rabbit = new Rabbit(scene);
+    //rabbit.setPosition(5, 5);
 });
+
+// When the map has been loaded, process it.
+//document.addEventListener("MAP:LOADED", function(event) {
+//    console.log(scene);
+//
+//    // Update function.
+//    let update = function update(dt) {
+//        //rabbit.update(dt);
+//    };
+//
+//    // Render function.
+//    let render = function render(dt) {
+//        // Clear screen.
+//        scene.ctx.clearRect(0, 0, scene.ctx.canvas.width, scene.ctx.canvas.height);
+//
+//        // Render grid.
+//        //scene.grid.render();
+//
+//        // Render entities.
+//        //rabbit.render();
+//    };
+//
+//    // Create new game loop.
+//    const gl = new GameLoop(update, render);
+//
+//    // Start game loop.
+//    gl.start();
+//});
--- src/modules/map.js
+++ src/modules/map.js
@@ -1,7 +1,52 @@
+import { get } from '../utils.js';
+
 class Map {
-    constructor(scene, name) {
+    constructor(scene) {
         this.scene = scene;
-        console.log(scene, name);
+
+        document.addEventListener("MAP:LOADED", (event) => {
+            this.processMap();
+        });
+    }
+
+    /**
+     * Loads the given map from /assets/maps/[name].json
+     * @param name
+     */
+    load(name) {
+        const path = '/grid-mas/assets/maps/' + name + '.json';
+        get(path).then((response) => {
+            // Add map data to scene.
+            this.scene.map = JSON.parse(response);
+
+            // Fire MAP:LOADED event.
+            document.dispatchEvent(new Event('MAP:LOADED'));
+
+        }, (error) => {
+            console.error("Failed to load map `" + name + "` from `" + path + "`.");
+        });
+    }
+
+    processMap() {
+        // Load tilesets.
+        console.log(this.scene);
+        const tilesetImage = this.scene.map.tilesets[0];
+        const tilesetImageUrl = tilesetImage.image.split('/').pop();
+        console.log(tilesetImage, tilesetImageUrl);
+
+        this.scene.ctx.canvas.width = tilesetImage.imagewidth;
+        this.scene.ctx.canvas.height = tilesetImage.imageheight;
+
+        tilesetImage.img = new Image();
+        tilesetImage.img.src = '/grid-mas/assets/spritesheets/' + tilesetImageUrl;
+        tilesetImage.img.onload = () => {
+            this.scene.ctx.drawImage(tilesetImage.img, 0, 0);
+        };
+
+
+
+        // Load image into canvas.
+
     }
 }
 
